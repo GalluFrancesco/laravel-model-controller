84a19584be755347b8d38292a50a9239b840aa28
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-03-06 17:41:19 +0100

    Add: style

--- resources/views/partials/cardmovie.blade.php
+++ resources/views/partials/cardmovie.blade.php
@@ -1,7 +1,7 @@
-<div class="card">
-    <div><b>Titolo:</b> {{$movie->title}}</div>
-    <div><b>Titolo Originale:</b> {{$movie->original_title}}</div>
-    <div><b>Nazionalità:</b> {{$movie->nationality}}</div>
-    <div><b>Data di Uscita:</b> {{$movie->date}}</div>
-    <div><b>Voto:</b> {{$movie->vote}}</div>
+<div class="card p-2">
+    <div class="my-1"><b>Titolo:</b> {{$movie->title}}</div>
+    <div class="my-1"><b>Titolo Originale:</b> {{$movie->original_title}}</div>
+    <div class="my-1"><b>Nazionalità:</b> {{$movie->nationality}}</div>
+    <div class="my-1"><b>Data di Uscita:</b> {{$movie->date}}</div>
+    <div class="my-1"><b>Voto:</b> {{$movie->vote}}</div>
 </div>
